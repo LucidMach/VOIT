54efffb58edc1a5ace68382615fd610830a7f4ec
LucidMach <nukalasuraj7@gmail.com>
2021-04-15 09:44:46 +0530

    fixing breaking errors for hardware integration

--- server/public/mic.js
+++ server/public/mic.js
@@ -76,11 +76,11 @@ function handleCommands(input) {
 }
 
 const transferCommands = async (command) => {
-  if (command === "on") command = 1;
-  if (command === "off") command = 0;
-  if (command === "crawl") command = 2;
-  if (command === "walk") command = 3;
-  if (command === "run") command = 4;
+  if (command === "on") command = "1";
+  if (command === "off") command = "0";
+  if (command === "crawl") command = "2";
+  if (command === "walk") command = "3";
+  if (command === "run") command = "4";
 
   const res = await fetch("../command", {
     method: "POST",
