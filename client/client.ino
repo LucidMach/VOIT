c29a3cbe8203068ec4c14518d9559986f58793ba
Nukala Suraj <nukalasuraj7@gmail.com>
2021-06-21 13:14:16 +0530

    inoDocs

--- client/client.ino
+++ client/client.ino
@@ -2,9 +2,11 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
+// My WiFi Credentials, Obvi Don't Copy LoL
 #define ssid "Nukalas_2GEXT"
 #define pass "885623277"
 
+// Led Pin + Blink Delay definitions
 #define led 2
 int u = 500;
 
@@ -14,6 +16,7 @@ void setup()
 {
   Serial.begin(115200);
 
+  // Connecting to WiFI
   WiFi.begin(ssid, pass);
   Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -24,7 +27,7 @@ void setup()
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-
+  // Configuring LED
   pinMode(led, OUTPUT);
 }
 
@@ -36,14 +39,18 @@ void loop()
     HTTPClient http;
     // Update SSL FingerPrint Regulaly
     http.begin(serverName.c_str(),"C3 75 80 CA 88 E2 40 62 94 8F E9 E9 BD 18 81 B8 33 39 AA 33");
+    // Making a GET req to server, Expects > 0 for success, < 0 for failure
     int httpResponseCode = http.GET();
-
+    
     if (httpResponseCode > 0)
     {
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
+      // Parsing body from JSON res
       String payload = http.getString();
       Serial.println(payload);
+      
+      // Conditional Exectution for Different Payloads
       if (payload == "1")
       {
         digitalWrite(led, LOW);
@@ -75,9 +82,11 @@ void loop()
     }
     else
     {
+      // InCase Unable to Connect to Server
       Serial.print("Error code: ");
       Serial.println(httpResponseCode);
     }
+    // EOL (HaHa Tron Reference)
     http.end();
   }
   else
@@ -85,6 +94,8 @@ void loop()
     Serial.println("WiFi Disconnected");
   }
 }
+
+// Blink Helper Function
 void ledBlink(int v)
 {
   digitalWrite(led, LOW);
